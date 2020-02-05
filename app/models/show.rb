e81711825aecc42fb6e0bc14ce6a7de50c72b498
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-05 18:12:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,10 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
+    self.where("rating = ?", self.highest_rating).first
+  end
+  
+  def self.highest_rating
     self.maximum(:rating)
   end
   

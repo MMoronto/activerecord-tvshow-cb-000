1237b61ac8d344883a90a59e785571381c207dae
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-02-05 18:14:01 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,7 +9,11 @@ class Show < ActiveRecord::Base
   end
   
   def self.lowest_rating
-    self.minimum(:rating).first
+    self.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    self.where("rating = ?", self.lowest_rating).first
   end
   
 end
